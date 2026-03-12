7af6b6322b4fe4be94e4e5493afcf75ade7c2b6b
Ariel Jose Luiz <joseluizariel@gmail.com>
2026-03-12 18:48:57 +0000

    Corrige detector de IP

--- app.py
+++ app.py
@@ -30,6 +30,45 @@ if st.button("Localizar IP"):
     except:
         st.error("Não foi possível localizar o IP")
 
+# ======================
+# Detector de golpes
+# ======================
+
+st.header("🧠 Detector de golpe")
+
+texto = st.text_area("Cole a mensagem suspeita")
+import streamlit as st
+import requests
+
+st.set_page_config(page_title="Escudo Digital IA")
+
+st.title("🛡️ ESCUDO DIGITAL IA")
+st.write("Detector de golpes + análise de IP")
+
+# ======================
+# Detector de IP
+# ======================
+
+st.header("🌍 Detector de localização do IP")
+
+ip = st.text_input("Digite domínio ou IP")
+
+if st.button("Localizar IP"):
+
+    try:
+        r = requests.get(f"http://ip-api.com/json/{ip}")
+        data = r.json()
+
+        st.success("IP encontrado")
+
+        st.write("IP:", data["query"])
+        st.write("País:", data["country"])
+        st.write("Cidade:", data["city"])
+        st.write("Organização:", data["org"])
+
+    except:
+        st.error("Não foi possível localizar o IP")
+
 if st.button("Localizar IP"):
 
     try:
